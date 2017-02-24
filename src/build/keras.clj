ac95793f467b1f01f2e1b86e9018b56d84654adc
Jason Owens <jason.l.owens.civ@mail.mil>
2017-02-23 22:40:55 -0500

    add ignored file under the  directory... maybe should be renamed

---
+++ src/build/keras.clj
@@ -0,0 +1,8 @@
+(ns build.keras)
+
+(def spec {:name "keras-with-ros"
+           :creator "Jason L. Owens <jason.l.owens.civ@mail.mil>"
+           :description "A Keras development environment with the following
+           additional packages: PCL, mlpack"
+           :base "nvidia/cuda:8.0-cudnn5-devel-ubuntu16.04"
+           :depends #{:keras :pcl :mlpack}})
